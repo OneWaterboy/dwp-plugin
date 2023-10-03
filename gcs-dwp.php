4312c5cc62e570eaaa32c975f4205f46ba533187
OneWaterboy <abrahmrollins@gmail.com>
2023-10-03 11:59:24 -0700

    Adding in content for Admin Page

--- gcs-dwp.php
+++ gcs-dwp.php
@@ -22,16 +22,61 @@ function builder_add_dwp_menu_page(){
       'DWP',
       'DWP',
       'manage_options',
-      'dwp',
-      'builder_dwp_menu_page_callback',
-      plugin_dir_url( __FILE__ ) . "assets/images/social-media.png"
+      'dwp-plugin',
+      'builder_render_admin_page',
+      plugin_dir_url(__FILE__) . '/assets/images/social-media.png',
+      80 
     );
   }
-  add_action('admin_menu', 'builder_add_dwp_menu_page');
+
+  add_action('admin_menu', 'builder_add_dwp_menu');
+
+
   
-  /**
-   * Callback function for the DWP menu page.
-   */
-  function builder_dwp_menu_page_callback(){
-    // Add code here to display the content of the DWP menu page
+  // Create sub menu items for Admin Versions of Posts under DWP Menu Item
+  function builder_add_submenu_pages() {
+    add_submenu_page(
+      'dwp-plugin', 
+      'Social Posts', 
+      'Social Posts', 
+      'manage_options', 
+      'edit.php?post_type=social_posts_admin'
+    );
+
+    add_submenu_page(
+        'dwp-plugin', 
+        'Emailers', 
+        'Emailers', 
+        'manage_options', 
+        'edit.php?post_type=emailers_admin'
+      );
   }
+  add_action('admin_menu', 'builder_add_submenu_pages');
+
+  // Render the admin page for the custom item
+function builder_render_admin_page() {
+    ?>
+    <div class="wrap">
+        <div class="content-types">
+            <div class="edit-post-type">
+                <div class="post-type-box">
+                    <img src="<?php echo plugin_dir_url(__FILE__) . '/assets/images/social-media-color.png'; ?>">
+                    <div class="post-type-link"></div>
+                </div>
+            </div>
+            <div class="edit-post-type">
+                <div class="post-type-box">
+                    <img src="">
+                    <div class="post-type-link"></div>
+                </div>
+            </div>
+            <div class="edit-post-type">
+                <div class="post-type-box">
+                    <img src="">
+                    <div class="post-type-link"></div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <?php
+}
