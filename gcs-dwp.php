563a35c82278b2dd13fc0e43e8b51b28cf234fc6
OneWaterboy <abrahmrollins@gmail.com>
2023-10-03 09:45:55 -0700

    Added Main Plugin File with Headers to allow activation

---
+++ gcs-dwp.php
@@ -0,0 +1,11 @@
+<?php
+/**
+* Plugin Name: Digital Web Package
+* Plugin URI: https://gardencentersolutions.com
+* Description: Plugin for running the custom functionality of the Digital Web Package proprietary system for Garden Center Solutions
+* Version: 1
+* Author: Abrahm Rollins
+* Requires at least: 6.3
+* Requires PHP: 8
+* Author URI: https://gardencentersolutions.com
+**/
