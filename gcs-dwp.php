314ca3437b621a27b70fd3002eff962844809977
OneWaterboy <abrahmrollins@gmail.com>
2023-10-03 10:03:52 -0700

    Added menu page to admin with custom icon

--- gcs-dwp.php
+++ gcs-dwp.php
@@ -9,3 +9,26 @@
 * Requires PHP: 8
 * Author URI: https://gardencentersolutions.com
 **/
+
+
+/**
+ * Add menu page called DWP to the admin menu.
+ */
+function cwpai_add_dwp_menu_page(){
+    add_menu_page(
+      'DWP',
+      'DWP',
+      'manage_options',
+      'dwp',
+      'cwpai_dwp_menu_page_callback',
+      plugin_dir_url( __FILE__ ) . "assets/images/social-media.png"
+    );
+  }
+  add_action('admin_menu', 'cwpai_add_dwp_menu_page');
+  
+  /**
+   * Callback function for the DWP menu page.
+   */
+  function cwpai_dwp_menu_page_callback(){
+    // Add code here to display the content of the DWP menu page
+  }
